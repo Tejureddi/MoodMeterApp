73bd4be6c40c3035d0bffdd3c5b5f8d5c94b3800
Tejureddi <84326694+Tejureddi@users.noreply.github.com>
2021-10-14 08:18:37 -0500

    main activity

--- app/src/main/java/com/example/moodmeterapp/MainActivity.java
+++ app/src/main/java/com/example/moodmeterapp/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.moodmeterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailET, passwordET;
+    //TextView authStatusTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailET = findViewById(R.id.editTextEmail);
+        passwordET = findViewById(R.id.editTextPass);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            authStatusTV.setText("onStart reloaded and " + currentUser.getEmail() + " is logged in");
+            // Take any action needed here when screen loads and a user is logged in
+        }
+        else {
+            authStatusTV.setText("onStart reloaded and user is null");
+            // Take any action needed here when screen loads and a user is NOT logged in
+        }
+    }
+
+    public void handleAuthChange(View v) {
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+        Log.i("Denna",  email + " " + password);
+
+        switch (v.getId()) {
+            case R.id.signInButton:
+                signIn(email, password);
+                //
+                break;
+            case R.id.signUpButton:
+                //
+                signUp(email, password);
+                break;
+            case R.id.signOutButton:
+                //
+                signOut(email,password);
+                break;
+        }
+    }
+
+
 }
