328c6441d2575ffffcde608395385f2d795ae1fc
Tejureddi <84326694+Tejureddi@users.noreply.github.com>
2021-10-25 08:18:13 -0500

    buttons

--- app/src/main/java/com/example/moodmeterapp/MoodPickerActivity.java
+++ app/src/main/java/com/example/moodmeterapp/MoodPickerActivity.java
@@ -12,13 +12,40 @@ public class MoodPickerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_picker);
+
     }
 
     public void blueButton(View view) {
         Intent intent = new Intent(MoodPickerActivity.this, DisplayCalenderActivity.class);
         startActivity(intent);
 
+
     }
+
+    public void greenButton(View view) {
+        Intent intent = new Intent(MoodPickerActivity.this, DisplayCalenderActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void yellowButton(View view) {
+        Intent intent = new Intent(MoodPickerActivity.this, DisplayCalenderActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void redButton(View view) {
+        Intent intent = new Intent(MoodPickerActivity.this, DisplayCalenderActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void resourceButton(View view) {
+        Intent intent = new Intent(MoodPickerActivity.this, ResourcesActivity.class);
+        startActivity(intent);
+
+    }
+
 
 
 }
