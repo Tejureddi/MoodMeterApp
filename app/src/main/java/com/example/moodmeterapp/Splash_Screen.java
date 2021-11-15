9303673ef92ce3bc2fd606850d0165cd5fc1114f
shiv-k28 <84193065+shiv-k28@users.noreply.github.com>
2021-11-14 21:22:19 -0600

    added splash screens + xml

---
+++ app/src/main/java/com/example/moodmeterapp/Splash_Screen.java
@@ -0,0 +1,29 @@
+package com.example.moodmeterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Screen extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 8000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash_Screen.this, MainActivity.class);
+                Splash_Screen.this.startActivity(mainIntent);
+                Splash_Screen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
